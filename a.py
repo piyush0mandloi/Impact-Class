7510b8769fbe1467fe5813dca4f9375cf09e47b3
Piyush Mandloi <piyushmandloi474@gmail.com>
2025-05-20 10:35:34 +0530

    Week3 Day 2 Completed

--- a.py
+++ a.py
@@ -1,3 +1,10 @@
-x = 10111
-i = 3
-print(x>>i | 1)
+s = "abcdeffedcba"
+i,j = 0, len(s)-1
+while i<j:
+    if s[i]!=s[j]:
+        break
+    i += 1
+    j-=1
+
+if i>=j:
+    print("YES")
--- week3_day1.java
+++ week3_day1.java
@@ -1,7 +1,55 @@
 public class week3_day1 {
     public static void main(String[] args) {
-        System.out.println();
-    }
-}
- 
+        // System.out.println();
 
+        // String str = "Hello World";
+        // System.out.println(str); // prints 'o'
+
+        // String str2 = str.substring(0, 5); // prints 'Hello'
+        // System.out.println(str2);
+        // System.out.println(str.indexOf("o"));
+
+        // System.out.println(str.concat(str2)); // prints 'Hello WorldHello'
+
+        // String s = "pappafap";
+        // int count = 0;
+        // for(int i=0; i<s.length(); i++){
+        //     char c = s.charAt(i);
+        //     if(c=='p'){
+        //         count++;
+        //     }
+        // }     
+        // System.out.println(count);
+
+        // String str = "vamsc the bad guy";
+        // int count2 = 0;
+        // for(int i=0; i<str.length(); i++){
+        //     char c = str.charAt(i);
+        //     if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
+        //         count2++;
+        //     }
+        // }
+        // System.out.println(count2);
+
+        String s = "abcdeffedcba";
+        int i = 0;
+        int j = s.length() - 1;
+        int check = 1;
+        while(i<j){
+            if(s.charAt(i) != s.charAt(j)){
+                check = 0;
+                break;
+            }
+            i++;
+            j--;
+        }
+        if(check ==1){
+            System.out.println("YEs");
+        } else {
+            System.out.println("NO");
+        }
+
+
+
+    }
+}   
